23314b62d827e337877a8b770884577c09a18b71
Abdeljalil37 <widadpop@gmail.com>
2023-11-10 15:58:40 +0100

    save function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,3 +29,10 @@ class FileStorage:
         ocname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
+    def save(self):
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
