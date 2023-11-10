aecdcf210a7489f68b34913885a0b77653994194
Abdeljalil37 <widadpop@gmail.com>
2023-11-10 15:56:58 +0100

    new function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,3 +23,9 @@ class FileStorage:
     def all(self):
         """Return the dictionary __objects."""
         return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
