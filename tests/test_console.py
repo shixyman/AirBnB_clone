867fbc73dc33ea03aecf23e6de203c6f61397d27
Abdeljalil37 <widadpop@gmail.com>
2023-11-12 00:05:20 +0100

    rename test console & add test prompt of hbnb cmd

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import unittest
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
